d608388d5d63b6082317f6451b3ea72427e9cad3
v1c7or20 <victorpa793@gmail.com>
2019-10-17 13:41:00 -0500

    skull

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int toDecimal(string word){
+    int pot = 1;
+    int rpt= 0;
+    for (auto i = word.rbegin(); i != word.rend() ; i++) {
+        if(isdigit(*i)){
+            rpt= rpt + atoi(&(*i))*pow(36,pot);
+            pot++;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   string a,b;
+    while (cin>>a>>b){
+       cout<<a<<" "<<b<<endl;
+   }
 }
